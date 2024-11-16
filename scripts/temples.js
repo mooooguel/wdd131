1ab5ea38d40dbbe60672a2f2896684a83abd188f
mooooguel <michaelmakesmusic25@gmail.com>
2024-11-16 16:55:59 -0700

    temples edit

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,13 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuButton = document.getElementById("menu");
     const navMenu = document.querySelector("nav ul");
 
-    // Toggle navigation menu on hamburger click
     menuButton.addEventListener("click", () => {
         navMenu.classList.toggle("show");
         menuButton.classList.toggle("show");
     });
 
-    // Footer dynamic content
     const yearSpan = document.getElementById("currentyear");
     const lastModifiedSpan = document.getElementById("lastModified");
 
